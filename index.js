4df9f90074cb61df7ae7b736d6b9407b9ec565fd
Roshan Choxi <roshan.choxi@gmail.com>
2017-01-31 11:47:02 -0800

    remove electron-reload

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
-require('electron-reload')(__dirname);
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
