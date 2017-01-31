f079da10a3936a81c2acfd12a1c56d35cff7e536
Roshan Choxi <roshan.choxi@gmail.com>
2017-01-30 17:34:59 -0800

    use unique key ids

--- js/instance.js
+++ js/instance.js
@@ -1,21 +1,36 @@
-var AWS    = require("aws-sdk");
-var config = JSON.parse(fs.readFileSync("config.json"));
+var AWS     = require("aws-sdk");
+var uuid    = require("uuid/v4");
+var glob    = require("glob");
+var config  = JSON.parse(fs.readFileSync("config.json"));
 
 module.exports = function() {
   this.ec2 = new AWS.EC2(config.ec2);
-  if(!fs.existsSync("Box.pem")) {
-    this.createAndSaveKeyPair();
+  this.findOrCreateKey(function(keyName) {
+    console.log("Key Name: " + keyName);
+  });
+}
+
+module.exports.prototype.findOrCreateKey = function(callback) {
+  var keyFiles = glob.sync("*.pem");
+
+  if(keyFiles.length > 0) {
+    var keyName = keyFiles[0].match(/(.+)\.pem/)[1]
+    callback(keyName);
+  } else {
+    this.createAndSaveKeyPair(callback);
   }
 }
 
-module.exports.prototype.createAndSaveKeyPair = function() {
-  this.ec2.createKeyPair({ KeyName: "Box" }, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else {
-      fs.writeFile("Box.pem", data.KeyMaterial, function(err) {
-        if(err) return console.log(err);
+module.exports.prototype.createAndSaveKeyPair = function(callback) {
+  var keyId   =  uuid();
+  var keyName = "Box-" + keyId;
 
-        console.log("The file was saved!");
+  this.ec2.createKeyPair({ KeyName: keyName }, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else {
+      fs.writeFile(keyName + ".pem", data.KeyMaterial, function(err) {
+        if(err) return console.log(err);
+        callback(keyName);
       });
     }
   });
