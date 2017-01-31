42235d8d618e46c921fc0b899af38a193c52ffa4
Roshan Choxi <roshan.choxi@gmail.com>
2017-01-30 17:11:12 -0800

    find or create key on instance initialization

--- js/instance.js
+++ js/instance.js
@@ -3,10 +3,13 @@ var config = JSON.parse(fs.readFileSync("config.json"));
 
 module.exports = function() {
   this.ec2 = new AWS.EC2(config.ec2);
+  if(!fs.existsSync("Box.pem")) {
+    this.createAndSaveKeyPair();
+  }
 }
 
 module.exports.prototype.createAndSaveKeyPair = function() {
-  ec2.createKeyPair({ KeyName: "Box" }, function(err, data) {
+  this.ec2.createKeyPair({ KeyName: "Box" }, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
     else {
       fs.writeFile("Box.pem", data.KeyMaterial, function(err) {
