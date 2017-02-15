bfa536d0be6b7a0e55257d1bd1f6d663a2619918
Roshan Choxi <roshan.choxi@gmail.com>
2017-02-14 17:33:26 -0800

    refactor instance.js to use class keyword

--- js/instance.js
+++ js/instance.js
@@ -9,136 +9,140 @@ var config  = JSON.parse(fs.readFileSync(app.getAppPath() + "/config.json"));
 var SSH     = require("simple-ssh");
 var log     = require('electron-log');
 
-module.exports = function() {
-  log.info("Creating Instance...");
-  this.ec2 = new AWS.EC2(config.ec2);
-  this.findOrCreateKey(this.createInstance.bind(this));
-  return this;
-}
-
-module.exports.prototype.createInstance = function(keyName) {
-  var params = {
-    ImageId: "ami-0b33d91d",
-    InstanceType: "t2.micro",
-    MaxCount: 1,
-    MinCount: 1,
-    KeyName: keyName,
-    SecurityGroupIds: ["sg-0a2ea676"]
+class Instance {
+  constructor() {
+    log.info("Creating Instance...");
+    this.ec2 = new AWS.EC2(config.ec2);
+    this.findOrCreateKey(this.createInstance.bind(this));
+    return this;
   }
 
-  this.ec2.runInstances(params, function(err, data) {
-    if (err) log.error(err, err.stack); // an error occurred
-    else {
-      this.reservation = data;
+  createInstance(keyName) {
+    var params = {
+      ImageId: "ami-0b33d91d",
+      InstanceType: "t2.micro",
+      MaxCount: 1,
+      MinCount: 1,
+      KeyName: keyName,
+      SecurityGroupIds: ["sg-0a2ea676"]
     }
-  }.bind(this));
-}
 
-module.exports.prototype.findOrCreateKey = function(callback) {
-  var keyFiles = glob.sync("*.pem");
-
-  if(keyFiles.length > 0) {
-    var keyName = keyFiles[0].match(/(.+)\.pem/)[1]
-    this.keyName = keyName;
-    callback(keyName);
-  } else {
-    this.createAndSaveKeyPair(callback);
-  }
-}
-
-module.exports.prototype.createAndSaveKeyPair = function(callback) {
-  var keyId   =  uuid();
-  var keyName = "Box-" + keyId;
-
-  this.ec2.createKeyPair({ KeyName: keyName }, function(err, data) {
-    if (err) log.error(err, err.stack);
-    else {
-      fs.writeFile(app.getAppPath() + "/" + keyName + ".pem", data.KeyMaterial, {mode: "400"}, function(err) {
-        if(err) return log.error(err);
-        this.keyName = keyName;
-        callback(keyName);
-      }.bind(this));
-    }
-  }.bind(this));
-};
-
-module.exports.prototype.remove = function(callback) {
-  this.ec2.stopInstances({ InstanceIds: this.instanceIds() }, function(err, data) {
-    callback();
-  }.bind(this));
-}
-
-module.exports.prototype.instanceIds = function() {
-  var instanceIds = [];
-
-  for(i=0; i < this.reservation.Instances.length; i++) {
-    let instance = this.reservation.Instances[i];
-    instanceIds.push(instance.InstanceId);
+    this.ec2.runInstances(params, function(err, data) {
+      if (err) log.error(err, err.stack); // an error occurred
+      else {
+        this.reservation = data;
+      }
+    }.bind(this));
   }
 
-  return instanceIds;
-}
+  findOrCreateKey(callback) {
+    var keyFiles = glob.sync("*.pem");
 
-//  Before we can SSH, we need to wait for:
-//
-//    - the EC2 reservation request to complete
-//    - the instance to change state to "running"
-//    - the instance to boot up its SSH server
-//
-module.exports.prototype.waitUntilRunning = function(callback) {
-  if(this.reservation === undefined) {
-    setTimeout(function() {
-      this.waitUntilRunning(callback);
-    }.bind(this), 1000);
-  } else {
-    log.info("Waiting for Instance to Start...")
-    this.pollInstanceState(callback);
-  }
-}
-
-module.exports.prototype.pollInstanceState = function(callback) {
-  this.ec2.describeInstances({ InstanceIds: this.instanceIds() }, function(err, data) {
-    this.reservation = data.Reservations[0];
-    if(!instancesReady(this.reservation)) {
-      setTimeout(function() { this.pollInstanceState(callback) }.bind(this), 1000);
+    if(keyFiles.length > 0) {
+      var keyName = keyFiles[0].match(/(.+)\.pem/)[1]
+      this.keyName = keyName;
+      callback(keyName);
     } else {
-      log.info("Waiting for SSH Connection...")
-      this.pollSSHConnection(callback);
+      this.createAndSaveKeyPair(callback);
     }
-  }.bind(this));
-}
-
-module.exports.prototype.keyPath = function() {
-  return app.getAppPath() + "/" + this.keyName + ".pem";
-}
-
-module.exports.prototype.pollSSHConnection = function(callback) {
-  let config = {
-    host: this.reservation.Instances[0].PublicIpAddress,
-    user: 'ec2-user',
-    key: fs.readFileSync(this.keyPath()),
-    timeout: 1000
   }
-  let ssh = new SSH(config);
 
-  ssh.exec("exit").start({
-    success: function() {
-      log.info("Instance Ready");
-      callback(this.keyPath(), this.reservation.Instances[0].PublicIpAddress);
-    }.bind(this),
-    fail: function() {
-      this.pollSSHConnection(callback);
-    }.bind(this)
-  });
+  createAndSaveKeyPair(callback) {
+    var keyId   =  uuid();
+    var keyName = "Box-" + keyId;
+
+    this.ec2.createKeyPair({ KeyName: keyName }, function(err, data) {
+      if (err) log.error(err, err.stack);
+      else {
+        fs.writeFile(app.getAppPath() + "/" + keyName + ".pem", data.KeyMaterial, {mode: "400"}, function(err) {
+          if(err) return log.error(err);
+          this.keyName = keyName;
+          callback(keyName);
+        }.bind(this));
+      }
+    }.bind(this));
+  }
+
+  remove(callback) {
+    this.ec2.stopInstances({ InstanceIds: this.instanceIds() }, function(err, data) {
+      callback();
+    }.bind(this));
+  }
+
+  instanceIds() {
+    var instanceIds = [];
+
+    for(let i=0; i < this.reservation.Instances.length; i++) {
+      let instance = this.reservation.Instances[i];
+      instanceIds.push(instance.InstanceId);
+    }
+
+    return instanceIds;
+  }
+
+  //  Before we can SSH, we need to wait for:
+  //
+  //    - the EC2 reservation request to complete
+  //    - the instance to change state to "running"
+  //    - the instance to boot up its SSH server
+  //
+  waitUntilRunning(callback) {
+    if(this.reservation === undefined) {
+      setTimeout(function() {
+        this.waitUntilRunning(callback);
+      }.bind(this), 1000);
+    } else {
+      log.info("Waiting for Instance to Start...")
+      this.pollInstanceState(callback);
+    }
+  }
+
+  pollInstanceState(callback) {
+    this.ec2.describeInstances({ InstanceIds: this.instanceIds() }, function(err, data) {
+      this.reservation = data.Reservations[0];
+      if(!instancesReady(this.reservation)) {
+        setTimeout(function() { this.pollInstanceState(callback) }.bind(this), 1000);
+      } else {
+        log.info("Waiting for SSH Connection...")
+        this.pollSSHConnection(callback);
+      }
+    }.bind(this));
+  }
+
+  keyPath() {
+    return app.getAppPath() + "/" + this.keyName + ".pem";
+  }
+
+  pollSSHConnection(callback) {
+    let config = {
+      host: this.reservation.Instances[0].PublicIpAddress,
+      user: 'ec2-user',
+      key: fs.readFileSync(this.keyPath()),
+      timeout: 1000
+    }
+    let ssh = new SSH(config);
+
+    ssh.exec("exit").start({
+      success: function() {
+        log.info("Instance Ready");
+        callback(this.keyPath(), this.reservation.Instances[0].PublicIpAddress);
+      }.bind(this),
+      fail: function() {
+        this.pollSSHConnection(callback);
+      }.bind(this)
+    });
+  }
 }
 
 function instancesReady(reservation) {
   let ready = true;
 
-  for(i=0; i < reservation.Instances.length; i++) {
+  for(let i=0; i < reservation.Instances.length; i++) {
     let instance = reservation.Instances[i];
     if(instance.State.Name !== "running") ready = false;
   }
 
   return ready;
 }
+
+module.exports = Instance 
