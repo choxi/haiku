f5dfbfc9f0bea7a5d419d80bae88e95db8955fae
Roshan Choxi <roshan.choxi@gmail.com>
2017-02-22 19:46:52 -0800

    minimal React example

--- js/application.js
+++ js/application.js
@@ -2,6 +2,13 @@ const app         = require('electron').remote.getGlobal("app")
 const ProgressBar = require('progressbar.js')
 const Instance    = require("./js/instance.js");
 const fs          = require("fs")
+const React       = require("react")
+const ReactDOM    = require("react-dom")
+
+ReactDOM.render(
+  React.createElement("h1", null, "Hello, world!"),
+  document.getElementById('root')
+)
 
 $(function() {
   if(fs.existsSync(app.getPath("appData") + "/Haiku/reservation.json")) {
