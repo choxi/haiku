443664f552f336b2968cb084c91fd0f3a469de13
Roshan Choxi <roshan.choxi@gmail.com>
2017-02-23 17:18:05 -0800

    use OpenMenu component to handle click event

--- js/application.jsx
+++ js/application.jsx
@@ -6,8 +6,82 @@ const React       = require("react")
 const ReactDOM    = require("react-dom")
 const OpenMenu    = require("./js/open-menu.jsx")
 
+function createInstance(params) {
+  var instance = new Instance(params)
+
+  $(".select-stack").hide()
+  $(".loading-screen").show()
+
+  var innerProgress = new ProgressBar.SemiCircle('.progress-bar .inner-bar', {
+    strokeWidth: 20,
+    color: "#FF8000"
+  })
+
+  var outerProgress = new ProgressBar.SemiCircle('.progress-bar .outer-bar', {
+    strokeWidth: 14,
+    color: "#D0E162"
+  })
+
+  $(".loading-status").text("Creating Instance...")
+  innerProgress.animate(0.30, {duration: 40000})
+  outerProgress.animate(0.25, {duration: 40000})
+
+  instance.on("starting", function() {
+    innerProgress.animate(0.65, {duration: 17000});
+    outerProgress.animate(0.5, {duration: 17000});
+    $(".loading-status").text("Starting...")
+  })
+
+  instance.on("connecting", function() {
+    innerProgress.animate(0.95, {duration: 17000});
+    outerProgress.animate(0.75, {duration: 17000});
+    $(".loading-status").text("Connecting...")
+  })
+
+  var term = new Terminal({ cursorBlink: true });
+  term.open(document.getElementsByClassName('terminal-wrapper')[0])
+  term.fit()
+
+  instance.on("ready", function() {
+    outerProgress.animate(1)
+    innerProgress.animate(1, function() {
+      $(".loading-screen").hide()
+      $(".terminal-wrapper").show()
+      term.fit();
+    }.bind(this))
+  })
+
+  instance.waitUntilRunning(function(keyPath, ipAddress) {
+    var sshLogin        = "ec2-user@" + ipAddress;
+    var sshArgs         = ["-oStrictHostKeyChecking=no", "-i", keyPath, sshLogin];
+    var pty             = require('pty').spawn("ssh", sshArgs, { name: "xterm-256color" });
+
+    pty.on("data", function(data) {
+      term.write(data);
+    });
+
+    term.on("data", function(data) {
+      pty.write(data)
+    })
+
+    term.on('resize', function (size) {
+      pty.resize(size.cols, size.rows)
+    })
+
+    term.focus();
+  });
+
+  window.addEventListener("resize", term.fit.bind(term))
+  window.onbeforeunload = function(event) {
+    if(instance.status !== "stopped") {
+      event.returnValue = false
+      instance.remove(window.close.bind(window)) 
+    }
+  }
+}
+
 ReactDOM.render(
-  <OpenMenu />,
+  <OpenMenu onSelect={createInstance} />,
   document.getElementById('open-menu')
 )
 
@@ -18,82 +92,4 @@ $(function() {
     $element.data("reservation", reservation)
     $(".select-instance").append($element)
   }
-
-  $(".stacks .ruby, .stacks .javascript, .stacks .tensorflow, .select-instance li").click(function(event) {
-    let $stack      = $(this)
-    let ami         = $stack.data("ami")
-    let reservation = $stack.data("reservation")
-    var instance    = new Instance({ami: ami, reservation: reservation});
-
-    $(".select-stack").hide()
-    $(".loading-screen").show()
-
-    var innerProgress = new ProgressBar.SemiCircle('.progress-bar .inner-bar', {
-      strokeWidth: 20,
-      color: "#FF8000"
-    })
-
-    var outerProgress = new ProgressBar.SemiCircle('.progress-bar .outer-bar', {
-      strokeWidth: 14,
-      color: "#D0E162"
-    })
-
-    $(".loading-status").text("Creating Instance...")
-    innerProgress.animate(0.30, {duration: 40000})
-    outerProgress.animate(0.25, {duration: 40000})
-
-    instance.on("starting", function() {
-      innerProgress.animate(0.65, {duration: 17000});
-      outerProgress.animate(0.5, {duration: 17000});
-      $(".loading-status").text("Starting...")
-    })
-
-    instance.on("connecting", function() {
-      innerProgress.animate(0.95, {duration: 17000});
-      outerProgress.animate(0.75, {duration: 17000});
-      $(".loading-status").text("Connecting...")
-    })
-
-    var term = new Terminal({ cursorBlink: true });
-    term.open(document.getElementsByClassName('terminal-wrapper')[0])
-    term.fit()
-
-    instance.on("ready", function() {
-      outerProgress.animate(1)
-      innerProgress.animate(1, function() {
-        $(".loading-screen").hide()
-        $(".terminal-wrapper").show()
-        term.fit();
-      }.bind(this))
-    })
-
-    instance.waitUntilRunning(function(keyPath, ipAddress) {
-      var sshLogin        = "ec2-user@" + ipAddress;
-      var sshArgs         = ["-oStrictHostKeyChecking=no", "-i", keyPath, sshLogin];
-      var pty             = require('pty').spawn("ssh", sshArgs, { name: "xterm-256color" });
-
-      pty.on("data", function(data) {
-        term.write(data);
-      });
-
-      term.on("data", function(data) {
-        pty.write(data)
-      })
-
-      term.on('resize', function (size) {
-        pty.resize(size.cols, size.rows)
-      })
-
-      term.focus();
-    });
-
-    window.addEventListener("resize", term.fit.bind(term))
-    window.onbeforeunload = function(event) {
-      if(instance.status !== "stopped") {
-        event.returnValue = false
-        instance.remove(window.close.bind(window)) 
-      }
-    }
-
-  });
-});
+})
--- js/open-menu.jsx
+++ js/open-menu.jsx
@@ -2,21 +2,31 @@ const React       = require("react")
 const ReactDOM    = require("react-dom")
 
 class OpenMenu extends React.Component {
+  constructor() {
+    super()
+    this.select = this.select.bind(this)
+  }
+
+  select(event) {
+    let ami = event.target.getAttribute("data-ami")
+    this.props.onSelect({ami: ami})
+  }
+  
   render() {
     return (
       <div className="select-stack">
         <ul className="select-instance"></ul>
         <h3> Select a Stack </h3>
         <div className="stacks">
-          <div className="ruby" data-ami="ami-165a0876">
+          <div className="ruby" data-ami="ami-165a0876" onClick={this.select}>
             <img src="./images/icon-ruby.png" />
             <p> Ruby </p>
           </div>
-          <div className="javascript" data-ami="ami-165a0876">
+          <div className="javascript" data-ami="ami-165a0876" onClick={this.select}>
             <img src="./images/icon-javascript.png" />
             <p> Node </p>
           </div>
-          <div className="tensorflow" data-ami="ami-165a0876">
+          <div className="tensorflow" data-ami="ami-165a0876" onClick={this.select}>
             <img src="./images/icon-tensorflow.png" />
             <p> Tensorflow </p>
           </div>
