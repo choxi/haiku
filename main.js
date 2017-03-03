8d5799bf5292685977cfcff505cf8ab92981aab3
Roshan Choxi <roshan.choxi@gmail.com>
2017-03-03 09:24:58 -0800

    switch to simpler electron-prebuilt-compile

--- main.js
+++
@@ -1,5 +0,0 @@
-var path = require('path');
-var electronCompile = require('electron-compile');
-
-let initScript = path.resolve(__dirname, 'index.js');
-electronCompile.init(__dirname, initScript);
