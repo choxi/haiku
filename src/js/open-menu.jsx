124f2f55e88d6f6ec863693cac7abc21c3a696a9
Roshan Choxi <roshan.choxi@gmail.com>
2017-03-11 18:40:49 -0800

    clean up open-menu.jsx

--- src/js/open-menu.jsx
+++ src/js/open-menu.jsx
@@ -1,7 +1,5 @@
 const React       = require("react")
 const ReactDOM    = require("react-dom")
-const fs          = require("fs")
-const app         = require('electron').remote.getGlobal("app")
 const Reservation = require("./reservation.js")
 const NewMenu     = require("./new-menu.jsx")
 
@@ -11,13 +9,10 @@ class OpenMenu extends React.Component {
     this.select               = this.select.bind(this)
     this.create               = this.create.bind(this)
     this.selectedClasses      = this.selectedClasses.bind(this)
-    this.disableCreateButton  = this.disableCreateButton.bind(this)
     this.disableOpenButton    = this.disableOpenButton.bind(this)
 
     this.state = { 
-      selection: {
-        reservation: null
-      }
+      reservation: null
     }
   }
 
@@ -30,11 +25,11 @@ class OpenMenu extends React.Component {
       reservation = Reservation.find(reservationId)
     }
 
-    this.setState({selection: {reservation: reservationId}})
+    this.setState({ reservation: reservationId })
   }
 
   selectedClasses(value) {
-    if(!!this.state.selection.reservation && (value === this.state.selection.reservation)) {
+    if(!!this.state.reservation && (value === this.state.reservation)) {
       return "selected"
     } else {
       return ""
@@ -42,46 +37,33 @@ class OpenMenu extends React.Component {
   }
 
   create(event) {
-    let params      = {}
-    let ami         = this.state.selection.ami
-    let reservation = this.state.selection.reservation
-
-    if(!!ami) {
-      params = { 
-        name: this.state.name, 
-        ami: ami 
-      }
-    } else if(!!reservation) {
-      params = {
-        name: reservation,
-        reservation: Reservation.find(reservation)
-      }
+    let name = this.state.reservation
+    let params = {
+      name:         name,
+      reservation:  Reservation.find(name)
     }
 
     this.props.onSelect(params)
   }
   
-  disableCreateButton() {
-    return !this.state.selection.ami || !this.state.name || this.state.name === ""
-  }
-
   disableOpenButton() {
-    return !this.state.selection.reservation
+    return !this.state.reservation
   }
   
   render() {
-    let selectInstance
     let reservations = Reservation.all()
+    let mappedReservations
 
     if(Object.keys(reservations).length !== 0) {
-      let mappedReservations = Object.keys(reservations).map(function (key) {
+      mappedReservations = Object.keys(reservations).map((key) => {
         return (<tr className={this.selectedClasses(key)} onClick={this.select} data-reservation={key} key={key}><td>{key}</td><td>Feb 24, 2017</td></tr>)
-      }.bind(this))
+      })
+    }
 
-      selectInstance = (
+    return (
+      <div className="select-stack">
         <div className="open-instance">
           <h3>Open a Saved Instance</h3>
-          {selectInstance}
 
           <table className="select-instance">
             <tbody>
@@ -92,16 +74,7 @@ class OpenMenu extends React.Component {
 
           <button disabled={this.disableOpenButton()} onClick={this.create}>Open</button>
         </div>
-      )
-    }
-
-    return (
-      <div className="select-stack">
-        {selectInstance}
-        <NewMenu />
-        <div className="new-instance">
-          <button disabled={this.disableCreateButton()} onClick={this.create} >Create</button>
-        </div>
+        <NewMenu onSelect={this.props.onSelect} />
       </div>
     )
   }
